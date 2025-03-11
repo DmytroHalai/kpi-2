df9b7cef325509b12dcadee117cd028ed41d84a7
DmytroHalai <dimagalai04@gmail.com>
2025-03-11 17:47:03 +0200

    Add logic to PostfixToInfix method

--- implementation.go
+++ implementation.go
@@ -1,9 +1,32 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // TODO: document this function.
 // PostfixToInfix converts
 func PostfixToInfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	var stack []string
+	parts := strings.Split(input, " ")
+
+	for _, part := range parts {
+		switch part {
+		case "+", "-", "*", "/", "^":
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid input")
+			}
+			op2, op1 := stack[len(stack)-1], stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			newExpr := fmt.Sprintf("(%s %s %s)", op1, part, op2)
+			stack = append(stack, newExpr)
+		default:
+			stack = append(stack, part)
+		}
+	}
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid input")
+	}
+	return stack[0], nil
 }
