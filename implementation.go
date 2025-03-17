7e4110199aa5d3ee4bff94c62ecc995030b64d08
DmytroHalai <dimagalai04@gmail.com>
2025-03-17 18:07:03 +0200

    Fix algorithm

--- implementation.go
+++ implementation.go
@@ -2,6 +2,7 @@ package lab2
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -17,9 +18,11 @@ import (
 //     are placed between operands and enclosed in parentheses to preserve operation order.
 //   - An error if the input is invalid (e.g., not enough operands for an operator).
 func PostfixToInfix(input string) (string, error) {
+	if !hasOperands(input) {
+		return "", fmt.Errorf("invalid input: %s", input)
+	}
 	var stack []string
-	parts := strings.Split(input, " ")
-
+	parts := strings.Fields(input)
 	for _, part := range parts {
 		switch part {
 		case "+", "-", "*", "/", "^":
@@ -31,11 +34,32 @@ func PostfixToInfix(input string) (string, error) {
 			newExpr := fmt.Sprintf("(%s %s %s)", op1, part, op2)
 			stack = append(stack, newExpr)
 		default:
-			stack = append(stack, part)
+			if !checkNum(part) {
+				return "", fmt.Errorf("invalid number: %s", part)
+			}
+			stack = append(stack, cleanNumber(part))
 		}
 	}
 	if len(stack) != 1 {
 		return "", fmt.Errorf("invalid input")
 	}
 	return stack[0], nil
+}
+
+func checkNum(input string) bool {
+	if _, err := strconv.ParseFloat(strings.Replace(input, ",", ".", 1), 64); err != nil {
+		return false
+	}
+	return true
+}
+
+func cleanNumber(input string) string {
+	if strings.ContainsAny(input, ".,") {
+		return "0" + strings.TrimLeft(input, "0")
+	}
+	return strings.TrimLeft(input, "0")
+}
+
+func hasOperands(input string) bool {
+	return strings.ContainsAny(input, "+-*/^")
 }
