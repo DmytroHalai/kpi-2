c5f246d944a835131ce7590e09a70e59479a95c3
IYuriev <ilya.yuriev.s@gmail.com>
2025-03-18 19:54:56 +0200

    Add flags for input and handle input errors

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
-
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"strings"
 )
 
 func main() {
-	handler := lab2.ComputeHandler{
-		Input:  os.Stdin,
-		Output: os.Stdout,
+	exprFlag := flag.String("e", "", "Postfix expression to convert")
+	fileFlag := flag.String("f", "", "Input file containing the postfix expression")
+	outputFlag := flag.String("o", "", "Output file to store the result")
+
+	flag.Parse()
+
+	if *exprFlag != "" && *fileFlag != "" {
+		fmt.Fprintln(os.Stderr, "Error: specify only one input source (-e or -f)")
+		os.Exit(1)
 	}
 
-	if err := handler.Compute(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
+	var input io.Reader
+	if *fileFlag != "" {
+		file, err := os.Open(*fileFlag)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error opening input file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		input = file
+	}
+	if *exprFlag != "" {
+		input = strings.NewReader(*exprFlag)
+	} else {
+		fmt.Fprintln(os.Stderr, "Error: no input provided (-e or -f required)")
 		os.Exit(1)
 	}
 }
